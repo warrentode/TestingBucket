2ca76e71e86bbaa3bf765770ed9695233eb0f5eb
Warren Tode <3997423+warrentode@users.noreply.github.com>
2023-08-12 18:03:34 -0400

    still testing something

--- PiglinMerchantMod/src/main/java/net/warrentode/piglinmerchantmod/entity/ai/behaviors/hunting/package-info.java
+++
@@ -1,8 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@FieldsAreNonnullByDefault
-package net.warrentode.piglinmerchantmod.entity.ai.behaviors.hunting;
-
-import net.minecraft.*;
-
-import javax.annotation.*;
